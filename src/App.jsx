8c14897d063d830518ceb75348d01e054c0547d7
ali-inReal <alim07922@gmail.com>
2023-03-25 13:48:24 +0500

    Membership Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 
-import { MembershipCard } from "./Components/MembershipCard/MembershipCard";
+import { MembershipCard } from "./MembershipCard/MembershipCard";
 function App() {
  
   return (
    <>
-     <MembershipCard textColor={""} rotation={true} width={"500px"} height={"500px"} rankColor={"#38b5de"} backgroundVideo={"/text.mp4"} flipCard={true}   joinDate="10 Feb,2002" memberName="Muhammad Ali" location="Lahore" memberId={2} memberRank="Senior" profileImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MCnvBDf0tSbcF6SmCshL-Fp-1ByLnJK09VPX_SVA&s" actOfKindness="Charity" />
+     <MembershipCard textColor={""} rotation={true} width={"500px"} height={"500px"} rankColor={"#38b5de"} backgroundVideo={"/blueLightning.mp4"} flipCard={true}   joinDate="10 Feb,2002" memberName="Muhammad Ali" location="Lahore" memberId={2} memberRank="Senior" profileImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MCnvBDf0tSbcF6SmCshL-Fp-1ByLnJK09VPX_SVA&s" actOfKindness="Charity" />
    </>
   );
 }
