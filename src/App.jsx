89c1e00c2bcc79c12f04a50d58a19518aae64a2d
ali-inReal <alim07922@gmail.com>
2023-03-31 06:21:44 +0500

    added imported object

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { MembershipCard } from "./MembershipCard/MembershipCard";
 import video from "./assets/blueLightning.mp4"
 import { WheelOfFortune } from "./wheelOfFortune";
 // import { Wheel } from "./wheelOfFortune/wheel";
+import { Spinner } from "./wheelOfFortune/Spinner";
 import { Canvas } from "@react-three/fiber";
 function App() {
   const segments = [
@@ -21,8 +22,11 @@ function App() {
       {/* <MembershipCard logo={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJnbIQgP-0AuA_AmRFKWmB5HHGMESIF6Z7FQ&usqp=CAU"} textColor={""} QRcode={"/qr.png"} rotation={true} width={"700px"} height={"700px"} rankColor={"#38b5de"} backgroundVideo={video} showBackSide={false} joinDate="10 Feb,2002" memberName="Muhammad Ali" location="Lahore" memberId={2} memberRank="Senior" profileImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MCnvBDf0tSbcF6SmCshL-Fp-1ByLnJK09VPX_SVA&s" actOfKindness="Charity" /> */}
        
        <Canvas >
+        <ambientLight/>
+       {/* <pointLight castShadow intensity={10} position={[0,0,10]}/> */}
        {/* <Wheel /> */}
-       <WheelOfFortune segments={segments}/>
+       {/* <WheelOfFortune segments={segments}/> */}
+       <Spinner/>
 </Canvas>
 
    
